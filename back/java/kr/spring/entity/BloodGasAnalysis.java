c379feecc03b0d21040db1732002e4598e2f67f0
GJ1 <dlrudwns0595@gmail.com>
2024-10-19 11:31:20 +0900

    1019push

---
+++ back/java/kr/spring/entity/BloodGasAnalysis.java
@@ -0,0 +1,40 @@
+package kr.spring.entity;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "bloodgas_analysis")
+public class BloodGasAnalysis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bloodidx")
+    private Long bloodIdx;
+
+    @Column(name = "pCO2")
+    private String pCO2;
+
+    @Column(name = "pH")
+    private String pH;
+
+    @Column(name = "pO2")
+    private String pO2;
+
+    @Column(name = "regdate")
+    private String regDate;
+
+    @ManyToOne
+    @JoinColumn(name = "bloodidx", referencedColumnName = "bloodidx", insertable = false, updatable = false)
+    private BloodCheck bloodCheck;
+}
---
+++ back/src/main/java/kr/spring/entity/BloodCheck.java
@@ -0,0 +1,48 @@
+package kr.spring.entity;
+
+import java.security.Timestamp;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bloodcheck")
+public class BloodCheck {
+    @Id
+    @Column(name = "bloodidx")
+    private Long bloodIdx;
+
+    @ManyToOne
+    @JoinColumn(name = "stayid")
+    private Visit visit;
+
+    @Column(name = "testname")
+    private String testName;
+
+    @Column(name = "testresult")
+    private String testResult;
+
+    @Column(name = "testunit")
+    private String testUnit;
+
+    @Column(name = "testtime")
+    private String testTime;
+
+    @Column(name = "diagnosis")
+    private String diagnosis;
+
+    @Column(name = "diagnosiscode")
+    private String diagnosisCode;
+
+    @Column(name = "regdate")
+    private String regDate;
+
+    // Getters and Setters
+}
