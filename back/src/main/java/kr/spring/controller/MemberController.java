cba8dfdc3ea1a15bcacb4ee59daa970562540211
GJ1 <dlrudwns0595@gmail.com>
2024-10-22 10:18:50 +0900

    please

--- back/src/main/java/kr/spring/controller/MemberController.java
+++ back/src/main/java/kr/spring/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class MemberController {
 
     @Autowired
     private MemberService service;
-
+    @PostMapping("/login")
     public ResponseEntity<?> memberLogin(@RequestBody Map<String, String> loginData, HttpSession session) {
         String username = loginData.get("username");
         String password = loginData.get("password");
