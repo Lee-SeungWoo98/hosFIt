656920de3a426915ea21777ea0096e0b0d2d2b50
Lee seung woo <dlelddn98@naver.com>
2024-11-07 14:25:04 +0900

    에러 로그

--- back/src/main/java/kr/spring/TestApplication.java
+++ back/src/main/java/kr/spring/TestApplication.java
@@ -1,11 +1,12 @@
+// src/test/java/kr/spring/TestApplication.java
 package kr.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class TestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(TestApplication.class, args);
     }
 }
