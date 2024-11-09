1b82dd61a7e25350aeb5239f01d47de65f452e39
Lee seung woo <dlelddn98@naver.com>
2024-11-09 16:51:39 +0900

    123frtrrtergw3e2rt45yr

--- front/jest.config.js
+++ front/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(axios|other-es6-module)/)', // axios 및 다른 ES6 모듈을 변환 대상으로 포함
+    '/node_modules/(?!(axios)/)', // axios 모듈을 변환 대상으로 포함
   ],
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy', // CSS 파일을 무시하기 위한 설정
