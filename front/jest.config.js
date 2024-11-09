d72b3bd10a22c4b1419a436be50f048a04eb2cab
Lee seung woo <dlelddn98@naver.com>
2024-11-09 16:38:29 +0900

    jset 수정

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,9 +1,11 @@
 // jest.config.js
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-      '/node_modules/(?!(axios)/)', // 'axios' 모듈을 변환 대상에 포함
-    ],
-  };
+  roots: ["<rootDir>/front/src"],  // 정확한 경로로 설정
+  testEnvironment: 'jsdom',   // jsdom 환경 설정 (필요 시)
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(axios)/)', // 'axios' 모듈을 변환 대상에 포함
+  ],
+};
