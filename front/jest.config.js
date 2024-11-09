4e0c326543e0f4700d103d61276340a3ea2d1b7a
[pppp1234] <[parkgangjun77@gmail.com]>
2024-11-09 14:41:45 +0900

    cicd수정중2

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,13 +1,20 @@
+// front/jest.config.js
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-      '/node_modules/(?!axios)/'
-    ],
+    rootDir: '.',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '^axios$': 'axios'
     },
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
+    transformIgnorePatterns: ['/node_modules/(?!axios)/'],
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest'
+    },
+    collectCoverageFrom: [
+      'src/**/*.{js,jsx}',
+      '!src/index.js',
+      '!src/reportWebVitals.js'
+    ],
+    coverageReporters: ['text', 'lcov']
   };
