b1374ce2ec510104326735eb9696eba907e6606b
Lee seung woo <dlelddn98@naver.com>
2024-11-09 15:16:09 +0900

    찐 간다.

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' }, modules: 'auto' }],
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-private-property-in-object'
+  ]
 };
--- front/src/setupTests.js
+++ front/src/setupTests.js
@@ -1,5 +1,36 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import 'jest-environment-jsdom';
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Mock IntersectionObserver
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  disconnect() {}
+  observe() {}
+  unobserve() {}
+};
+
+// localStorage Mock
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  },
+  writable: true,
+});
