a86b462186233948c38ab8fb65cdd3957e36c951
GJ1 <dlrudwns0595@gmail.com>
2024-11-19 20:13:58 +0900

    cccomit

--- backhosift/src/main/java/kr/spring/service/MemberService.java
+++ backhosift/src/main/java/kr/spring/service/MemberService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import com.sun.corba.se.spi.activation.Repository;
 
 @Service
 public class MemberService {
