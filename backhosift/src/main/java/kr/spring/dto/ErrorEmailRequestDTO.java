41d597fa05ba46c12d7f049d432b9342aa84b700
KiNAYEONG <gkhgudwp2020@gmail.com>
2024-11-20 12:20:46 +0900

    - 관리자 기능 복구

---
+++ backhosift/src/main/java/kr/spring/dto/ErrorEmailRequestDTO.java
@@ -0,0 +1,19 @@
+package kr.spring.dto;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class ErrorEmailRequestDTO {
+    private Long id;
+    private String errorname;
+    private String errormessage;
+    private String errorstack;
+    private String errortype;
+    private String severitylevel;
+    private String url;
+    private String userid;
+    private String browser;
+    private LocalDateTime createdat;
+    private Boolean isresolved;
+}
